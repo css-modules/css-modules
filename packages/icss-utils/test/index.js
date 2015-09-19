375919ccaeb833f9f929749241c74b80ccfacb75
Glen Maddern <glenmaddern@gmail.com>
2015-09-19 14:15:08 +0200

    fleshing out a few more tests

--- packages/icss-utils/test/index.js
+++ packages/icss-utils/test/index.js
@@ -23,8 +23,41 @@ describe('replace-symbols', () => {
     test('.foo { color: red }', {foo: 'bar'}, '.foo { color: red }')
   })
 
+  it('should not change property names', () => {
+    test('.foo { color: red }', {color: 'background'}, '.foo { color: red }')
+  })
+
   it('should change declaration values', () => {
     test('.foo { color: red }', {red: 'blue'}, '.foo { color: blue }')
+  })
+
+  it('should change symbols within declaration values', () => {
+    test('.foo { box-shadow: 0 0 0 4px red }', {red: 'blue'}, '.foo { box-shadow: 0 0 0 4px blue }')
+  })
+
+  it('should change multiple symbols within declaration values', () => {
+    test('.foo { box-shadow: top left blur spread color }',
+      {top: '1px', left: '2px', blur: '3px', spread: '4px', color: 'red'},
+      '.foo { box-shadow: 1px 2px 3px 4px red }')
+  })
+
+  it('should not replace a replacement', () => {
+    test('.foo { background: blue; color: red }',
+      {red: 'blue', blue: 'green'},
+      '.foo { background: green; color: blue }')
+  })
+
+  it('should not get trolled by me', () => {
+    test('.foo { color: white }',
+      {white: 'lightblue', blue: 'green'},
+      '.foo { color: lightblue }')
+    test('.foo { color: white }',
+      {white: 'light blue', blue: 'green'},
+      '.foo { color: light blue }')
+  })
+
+  it('should change media queries', () => {
+    test('@media small { .foo { color: red } }', {small: '(max-width: 599px)'}, '@media (max-width: 599px) { .foo { color: red } }')
   })
 })
 
